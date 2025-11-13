ff8135703ab5bf45f1c1a424c957c2854208a8f0
gabrielwsc14-dot <gabriel.wsc14>
2025-11-13 15:48:55 -0300

    AtualizaþÒo automßtica de vagas

---
+++ iniciar_bot.bat
@@ -0,0 +1,16 @@
+@echo off
+cd /d "C:\Job" 
+
+:: Atualiza repositório
+git pull
+
+:: Inicia o bot (sem travar o .bat)
+start /b "" python bot_vagas_pc.py
+
+:: Aguarda um tempo pro bot fazer a busca (opcional)
+timeout /t 30 >nul
+
+:: Envia alterações pro GitHub
+git add .
+git commit -m "Atualização automática de vagas"
+git push
